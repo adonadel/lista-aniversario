737315c2df4c3f33ade4f328b99254fc7f042bfd
Alexandre Donadel <xanded.fer2@gmail.com>
2023-09-18 21:36:18 -0300

    Criação do projeto

---
+++ src/main/java/com/example/listaaniversario/listaaniversario/ListaaniversarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.listaaniversario.listaaniversario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaaniversarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaaniversarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/listaaniversario/listaaniversario/ListaaniversarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listaaniversario.listaaniversario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaaniversarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
